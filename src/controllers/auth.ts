f2bc675972d8f3ceacd9e41a6db4e522b3d8c0be
kamauJeff <jeffreykamau50@gmail.com>
2025-12-02 18:17:38 +0300

    implemented login and log out to login and logout users

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,7 @@
 import { type Request, type Response } from "express";
 import bcrypt from 'bcryptjs'
 import { PrismaClient } from '@prisma/client';
+import jwt from "jsonwebtoken";
 
 
 
@@ -29,4 +30,59 @@ export const register = async(req: Request, res: Response) => {
         
     }
     
+}
+
+export const login = async function (req:Request, res:Response) {
+  try {
+    // get the identifier and password
+    const {identifier, password} = req.body
+    // get the user whose username or email match the identifier
+    const user= await client.user.findFirst({
+      where: {
+        OR: [{ emailAddress: identifier}, {username: identifier}]
+      }
+    })
+    // if user not found, we have wrong login credentials
+    if (!user) {
+      res.status(400).json({message: "Wrong Login Credentials"})
+        return;
+    }
+    
+    // if user found, compare the user password with the given password
+     const passwordMatch=  await bcrypt.compare(password, user.password)
+    // if passwords don't match - wrong credentials
+    if (!passwordMatch) {
+      res.status(400).json({message: "Wrong Login credentials"})
+      return;
+      
+    }
+    // if they match - login success - prepare payload
+     const payload = {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      username: user.username,
+      emailAddress: user.emailAddress,
+      avatar: user.avatar
+
+     }
+    // generate a token and send it to the client as acookie
+  const token = jwt.sign(payload, process.env.JWT_SECRET_KEY!, {expiresIn: `21d`});
+    res.status(200).cookie("authToken", token).json(payload)
+  } catch (error) {
+    res.status(500).json({message: "Something went wrong"})
+    
+  }
+  
+}
+
+export const logout = function (req:Request, res: Response) {
+  try {
+    res.status(200).clearCookie("authToken").json({message: "Successfully logged out"})
+    
+  } catch (error) {
+    res.status(500).json({message: "Something went wrong"})
+    
+  }
+  
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, {type Request, type Response, type NextFunction} from 'express';
-import {register} from './controllers/auth.ts'
+import {register, login, logout} from './controllers/auth.ts'
 import {checkDetails} from './middlewares/checkDetails.ts'
 import { checkUsernameAndEmail } from "./middlewares/checkUsernameAndEmail.ts";
 import { checkPasswordStrength } from "./middlewares/checkPasswordStrength.ts";
+import dotenv from "dotenv";
 
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
@@ -17,6 +19,9 @@ app.get("/", (_req, res) =>{
 
 
 app.post("/auth/register", checkDetails, checkUsernameAndEmail, checkPasswordStrength, register )
+app.post("/auth/login", login)
+app.post("/auth/logout", logout)
+
 
 const PORT = 5200;
 app.listen(PORT, function() {
